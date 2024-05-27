da70303acd4cf8b37d1cb56a433f11f85d3181ea
jesky <jesky.yeremi@gmail.com>
2024-05-28 04:58:41 +0700

    feat(card): added game logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Box, Grid, Typography } from '@mui/material';
 import { Card } from './components';
 
@@ -10,9 +10,42 @@ interface ListItemProps {
   revealed: boolean;
 }
 
-export default function Home() {
-  const [list, setList] = useState<ListItemProps[]>([]);
+interface TempProps {
+  id: string;
+  content: string | number;
+}
 
+export default function Home() {
+  // variables init
+  const [list, setList] = useState<ListItemProps[]>([]);
+  const [temp, setTemp] = useState<TempProps[]>([]);
+
+  useEffect(() => {
+    // game logic
+    if (temp.length == 2) {
+      if (temp[0].content != temp[1].content) {
+        // conceal the cards
+        setTimeout(() => {
+          setList((list) => {
+            return list.map((item) => {
+              if (item.id == temp[0].id || item.id == temp[1].id) {
+                return { ...item, revealed: false };
+              }
+
+              return item;
+            });
+          });
+        }, 500);
+      }
+
+      setTemp([]);
+    }
+
+    // end game logic
+    /* under development, continue write the codes here. the game will end when all the cards is revealed, it would show a game over message and restart game button */
+  }, [temp]);
+
+  // automatically fill the list with some static item
   if (list.length == 0) {
     setList((current) => {
       return [
@@ -27,11 +60,14 @@ export default function Home() {
     });
   }
 
-  const handleReveal = (id: string) => {
+  const handleReveal = (id: string, content: string | number) => {
+    if (temp.length < 2) setTemp((prev) => [...prev, { id, content }]);
+
+    // reveal the content
     setList((list) => {
       return list.map((item) => {
-        if (item.id == id) {
-          return { ...item, revealed: !item.revealed };
+        if (item.id == id && !item.revealed) {
+          return { ...item, revealed: true };
         }
 
         return item;
@@ -57,7 +93,11 @@ export default function Home() {
         {list.map((item) => {
           return (
             <Grid item xs key={item.id}>
-              <Card {...item} fluid onClick={() => handleReveal(item.id)} />
+              <Card
+                {...item}
+                fluid
+                onClick={() => handleReveal(item.id, item.content)}
+              />
             </Grid>
           );
         })}
